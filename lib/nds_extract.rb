c046f2742436e610c3645c0d7e74801d2ddbe0af
Nate Montgomery <kilikalbright@hotmail.com>
2020-01-10 11:59:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,7 @@ def gross_for_director(director_data)
    total = 0
   while movie_index < director_data[:movies].length do
     total += director_data[:movies][movie_index][:worldwide_gross]
-   movie_index +=1  
+    movie_index +=1  
   end
   
   total
